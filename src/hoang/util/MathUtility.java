8dd8bff39ea91177f8cec4aeb7082d37957029f3
RedKAa <nth8518@gmail.com>
2020-11-14 11:57:25 +0700

    1st upload

---
+++ src/hoang/util/MathUtility.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hoang.util;
+
+/**
+ *
+ * @author user
+ */
+public class MathUtility {
+    //làm bộ thư viện fake giống y chang Math
+    //Math.PI, .abs .sqrt .pow(a,b) 
+
+    public static final double PI = 3.1415;
+
+    //tinh giai thua, am giai thua vo nghia, 21! long ko tinh dc -> 0!..20!
+    public static long getFactorial(int n) {
+        if (n < 0 || n > 20) {
+            throw new IllegalArgumentException("n must be between 0..20!");
+        }
+        long result = 1;
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        for (int i = 2; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+//        return n*getFactorial(n-1);
+    
+    
+}
---
+++ src/math/util/MathUtil.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package math.util;
+
+import hoang.util.MathUtility;
+
+/**
+ *
+ * @author user
+ */
+public class MathUtil {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // test function by looking
+        long expected = 120;//result should be return by getFactorial(5)
+        long actual = MathUtility.getFactorial(5);
+        System.out.println("Expected: "+expected+", actual: "+actual);
+        
+        //expected: 720 if getFactorial(6)
+        System.out.println("6!: "+MathUtility.getFactorial(6));
+        //expected: 1 if getFactorial(0)
+        System.out.println("0!: "+MathUtility.getFactorial(0));
+        //expected: IllegalArgumentException if parameter is -5
+        System.out.println("-5!: "+MathUtility.getFactorial(-5));
+        
+        
+    }
+    
+}
